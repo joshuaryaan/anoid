1b4b0c6e29a71e60b982255fc7661f75d9bc980e
Josh Warner <joshua.ryan.warner@icloud.com>
2020-05-31 16:43:53 +1000

    menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function () { 
-
+    $( ".links a" ).hide();
+    $(".menu").click(function(){
+        $( ".links" ).fadeIn(300);
+        $( ".links a" ).fadeIn(800);
+    });
+    $(".close").click(function(){
+        $( ".links a" ).fadeOut(300);
+        $( ".links" ).fadeOut(800);
+    });
 });
